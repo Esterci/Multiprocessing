d75052ecf0b23e23324ade5eb7422b8fd199b73a
Esterci <thiago.esterci@engenharia.ufjf.br>
2020-09-16 14:28:31 -0300

    Create multiprocessing.py

---
+++ multiprocessing.py
@@ -0,0 +1,14 @@
+import multiprocessing
+import time
+
+start = time.perf_counter()
+
+def do_something():
+    print('Sleeping 1 second...')
+    time.sleep(1)
+    print('Done Sleeping...')
+
+
+finish = time.perf_counter()
+
+print(f'Finished in {round (finish-start,2)} second(s)')
